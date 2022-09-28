c64e9101cbf0de3e6cd13d2d9d21bba1051edc79
zhenyu <zfish97@outlook.com>
2022-09-28 16:31:19 +0800

    自定义光照

--- Unity/Lghtingproject/Assets/Shader/ShaderLib/ZY_ForwardPass.hlsl
+++ Unity/Lghtingproject/Assets/Shader/ShaderLib/ZY_ForwardPass.hlsl
@@ -2,24 +2,13 @@
 #define ZY_CUSTOM_FORWARDPASS
 
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
-
-#define _ALPHATEST_ON
 
 CBUFFER_START(UnityPerMaterial)
 float4 _BaseMap_ST;
 float4 _NoiseMap_ST;
 float4 _BaseColor;
 float4 _OcclusionColor;
-
-float _Distance;
-half __ForceScaleScale;
 half _AOIntensity;
-half _FurEdgeSoftness;
-half _FurSoftness;
-
-float3 _ForceDirection;
-
 #ifdef _ALPHATEST_ON
     float _Cutoff;
 #endif
@@ -27,40 +16,8 @@ CBUFFER_END
 
 TEXTURE2D(_BaseMap);   SAMPLER(sampler_BaseMap);
 TEXTURE2D(_NoiseMap);   SAMPLER(sampler_NoiseMap);
-TEXTURE2D(_FlowMap);   SAMPLER(sampler_FlowMap);
 
-half FUR_LAYER_OFFSET;
-
-struct Attributes
-{
-    float4 positionOS       : POSITION;
-    float2 uv               : TEXCOORD0;
-    float3 normal           : NORMAL;
-    float4 tangent          : TANGENT;
-    UNITY_VERTEX_INPUT_INSTANCE_ID
-};
-
-struct Varyings
-{
-    float4 vertex       : SV_POSITION;
-    float2 uv           : TEXCOORD0;
-    float fogCoord      : TEXCOORD1;
-    
-    float3 worldPos     : TEXCOORD2;
-    float3 viewDirWS    : TEXCOORD3;
-    float3 normalWS     : TEXCOORD4;
-
-    UNITY_VERTEX_INPUT_INSTANCE_ID
-    UNITY_VERTEX_OUTPUT_STEREO
-};
 // -----------------------------------------
-
-#pragma exclude_renderers gles glcore
-#pragma target 4.5
-
-#pragma vertex vert
-#pragma fragment frag
-
 
 Varyings vert(Attributes input)
 {
@@ -93,16 +50,11 @@ half4 frag(Varyings input) : SV_Target
     half2 uv = input.uv;
 
     Light mainLight = GetMainLight();
-    
-    half3 h = SafeNormalize(mainLight.direction + normalize(input.viewDirWS));
-    half nov = dot(normalize(input.normalWS) , normalize(input.viewDirWS));
-    half nol = dot(mainLight.direction,normalize(input.normalWS));
-    half noh = dot(h , normalize(input.normalWS));
-
+    ZYSurfaceData surfaceData = INIT_ZY_SURFACEDATA(input.uv);
     // caculate fresnel
     float fresnel = (1 - nov);
 
-    // temp caculate simplelight
+    // temp caculate simple light
     half3 sampleEnironmentSH = SampleSH(input.normalWS); // environmentlight;
     half3 lambertlight = LightingLambert(mainLight.color,mainLight.direction,input.normalWS);
     half specularTerm = pow(saturate(noh), 64) * mainLight.distanceAttenuation;
--- Unity/Lghtingproject/Assets/Shader/ShaderLib/ZY_Input.hlsl
+++ Unity/Lghtingproject/Assets/Shader/ShaderLib/ZY_Input.hlsl
@@ -1,4 +1,28 @@
 #ifndef ZY_CUSTOM_INPUT
 #define ZY_CUSTOM_INPUT
 
+struct Attributes
+{
+    float4 positionOS       : POSITION;
+    float2 uv               : TEXCOORD0;
+    float3 normal           : NORMAL;
+    float4 tangent          : TANGENT;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct Varyings
+{
+    float4 vertex       : SV_POSITION;
+    float2 uv           : TEXCOORD0;
+    float fogCoord      : TEXCOORD1;
+    
+    float3 worldPos     : TEXCOORD2;
+    float3 viewDirWS    : TEXCOORD3;
+    float3 normalWS     : TEXCOORD4;
+
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+    UNITY_VERTEX_OUTPUT_STEREO
+};
+
+
 #endif
